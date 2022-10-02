67debd5aea266b1543bfe48b5cb8663c1a9f6aef
Stephan Raab <sarcartist@gmail.com>
2022-10-02 13:13:13 -0400

    maui beginner mvvm

---
+++ LevelUpDarts/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace LevelUpDarts.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        Items = new ObservableCollection<string>();
+    }
+
+    [ObservableProperty]
+    ObservableCollection<String> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    void Add()
+    {
+        // add our item
+        if (!string.IsNullOrWhiteSpace(Text))
+        {
+            Items.Add(Text);
+        }
+
+        // reset text
+        Text = string.Empty;
+    }
+
+    [RelayCommand]
+    void Delete(string s)
+    {
+        if (Items.Contains(s))
+        {
+            Items.Remove(s);
+        }
+    }
+}
